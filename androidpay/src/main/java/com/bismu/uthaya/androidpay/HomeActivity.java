a6d7bd6e82c0c219ccc7cc5ed77f9436b169a0f0
uthayacloud <uthaya.uthaya031@gmail.com>
2017-08-01 18:08:01 +0530

    issue fix

--- androidpay/src/main/java/com/bismu/uthaya/androidpay/HomeActivity.java
+++ androidpay/src/main/java/com/bismu/uthaya/androidpay/HomeActivity.java
@@ -1,19 +1,19 @@
 package com.bismu.uthaya.androidpay;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity {
+    Activity activity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    HomeActivity (Activity activity) {
+        this.activity = activity;
     }
 
     public void showToast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
 
     }
 }
